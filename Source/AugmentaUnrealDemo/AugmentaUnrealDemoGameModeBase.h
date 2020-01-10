525598aa8710f361d04e3734d11844996927d9bc
Tejaswin G Macharla <tejaswin.m@gmail.com>
2020-01-10 15:29:53 -0500

    - Added  a blank Cpp UE4 project

---
+++ Source/AugmentaUnrealDemo/AugmentaUnrealDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AugmentaUnrealDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AUGMENTAUNREALDEMO_API AAugmentaUnrealDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
